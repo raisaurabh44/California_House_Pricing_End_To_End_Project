4e5aa62fadd65357acf5056d7401e78df67e0a4b
raisaurabh44 <raisaurabh44@gmail.com>
2026-01-19 00:27:08 +0530

    Web Page created and ready

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ pickle_model2 = pickle.load(open("models/RidgeCV.pkl", "rb"))
 pickle_model3 = pickle.load(open("models/RandomForestRegressor.pkl", "rb"))
 pickle_model4 = pickle.load(open("models/GradientBoostingRegressor.pkl", "rb"))
 
+FEATURE_NAMES=scaler.feature_names_in_
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template("home.html", best_model=None, predictions_table=[])
+    #return render_template('home.html')
 
 
 @app.route('/predict_api', methods=['POST'])
@@ -26,9 +29,10 @@ def predict_api():
     ## Getting Data from request Postman API call
     data = request.json['data']
     print(data)
-
+    
     ## Converting data into numpy array in the shape of 2D array required by sklearn models 
-    input_array = np.array(list(data.values())).reshape(1, -1)
+    #input_array = np.array(list(data.values())).reshape(1, -1)
+    input_array = pd.DataFrame([data], columns=FEATURE_NAMES)
     print(input_array)
 
     # Scaling input data
@@ -62,6 +66,50 @@ def predict_api():
         "best_model": best_model
     })
 
+@app.route('/predict', methods=['POST'])
+def predict():
+     # Read form input
+    data_dict = {name: float(val) for name, val in zip(FEATURE_NAMES, request.form.values())}
+    input_array = pd.DataFrame([data_dict], columns=FEATURE_NAMES)
+
+    # # Read form input
+    # data = [float(x) for x in request.form.values()]
+    # input_array = np.array(data).reshape(1, -1)
+
+    # Scale input
+    final_input = scaler.transform(input_array)
+
+    # Predict using all 4 models
+    pred_linear = float(pickle_model1.predict(final_input)[0])
+    pred_ridge  = float(pickle_model2.predict(final_input)[0])
+    pred_rf     = float(pickle_model3.predict(final_input)[0])
+    pred_gb     = float(pickle_model4.predict(final_input)[0])
+
+    # Store predictions
+    predictions_table = [
+        {"model": "LinearRegression", "prediction": float(pred_linear)},
+        {"model": "RidgeCV", "prediction": float(pred_ridge)},
+        {"model": "RandomForestRegressor", "prediction": float(pred_rf)},
+        {"model": "GradientBoostingRegressor", "prediction": float(pred_gb)}    
+    ]
+
+    # Select best model (highest prediction)
+    best_model = max(predictions_table, key=lambda x: x["prediction"])
+    # Pass variables to template
+    return render_template(
+        "home.html",
+        predictions_table=predictions_table,
+        best_model=best_model
+    )
+    # ["model"]
+    # best_prediction = max(predictions_table, key=lambda x: x["prediction"])["prediction"]
+    # return render_template(
+    #     "home.html",
+    #     prediction_text=(
+    #         f"Best Model: {best_model_name} | "
+    #         f"Predicted House Price: ${best_prediction:,.2f}"
+    #     )
+    #)
 
 if __name__ == "__main__":
     app.run(debug=True)     
